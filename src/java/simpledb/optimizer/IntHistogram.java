1483f827c73d4ae4ed2f38c40fc5c17dc2ec1190
Yunrui Zheng <pollery888@gmail.com>
2023-08-29 20:48:33 +0800

    optimal speed when finding bucket

--- src/java/simpledb/optimizer/IntHistogram.java
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -11,13 +11,11 @@ import java.util.TreeMap;
 public class IntHistogram {
 
     /**
-     * 直方图中每个桶抽看成一个对象比较好操作
+     * 直方图中每个桶看成一个对象比较好操作
      */
-    public class Bucket {
+    public static class Bucket {
         public double left;
         public double right;
-        public Bucket pre;
-        public Bucket next;
         public int height;
 
         public Bucket(){
@@ -28,12 +26,11 @@ public class IntHistogram {
         }
     }
 
-    private int numBuckets;
-    private int min;
-    private int max;
-    private double bWidth; // 桶宽
-    private Bucket histHead; // 头桶（哨兵）
-    private Bucket histTail; // 尾桶（哨兵）
+    private final int numBuckets;
+    private final int min;
+    private final int max;
+    private final double bWidth; // 桶宽
+    private final Bucket[] histData;
     private int ntups; // 元组总数量
 
     /**
@@ -58,10 +55,7 @@ public class IntHistogram {
         this.min = min;
         this.max = max;
         this.bWidth = (max-min) * 1.0 / buckets;
-        this.histHead = new Bucket();
-        this.histTail = new Bucket();
-        histHead.next = histTail;
-        histTail.pre = histHead;
+        this.histData = new Bucket[buckets];
         constructBuckets();
         this.ntups = 0;
     }
@@ -70,16 +64,12 @@ public class IntHistogram {
      * 初始化所有桶
      */
     private void constructBuckets(){
-        Bucket current = histHead;
+        int index = 0;
         for(double left = min; left < max; left += bWidth){
             Bucket newBucket = new Bucket();
             newBucket.left = left;
             newBucket.right = left + bWidth;
-            newBucket.pre = current;
-            newBucket.next = current.next;
-            current.next.pre = newBucket;
-            current.next = newBucket;
-            current = newBucket;
+            histData[index++] = newBucket;
         }
     }
 
@@ -89,20 +79,16 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
-        findBucket(v).height ++;
+        histData[findBucketIndex(v)].populate();
         ntups ++;
     }
 
-    private Bucket findBucket(int v){
-        Bucket current = histHead.next;
-        if(current.left > v) return histHead;
-        while(current != histTail){
-            if(v >= current.left && v < current.right){
-                return current;
-            }
-            current = current.next;
-        }
-        return histTail;
+    private int findBucketIndex(int v){
+        if(v < min) return -1; // 在区间左侧
+        if(v > max) return -2; // 在区间右侧
+        if(v == min) return 0;
+        if(v == max) return numBuckets-1; // 两端的情况特殊处理
+        return (int) Math.ceil((double)(v - min) / bWidth) - 1;
     }
 
     /**
@@ -119,67 +105,58 @@ public class IntHistogram {
     	// some code goes here
         double selectivity = -1.0;
         double temp;
-        Bucket bucket = findBucket(v);
-        switch (op){
+        int i = findBucketIndex(v);
+        // 注意查询的值不一定在最大最小区间内
+        // 不同情况的选择性不同
+        if(i < 0){
+            if(op == Predicate.Op.EQUALS || op == Predicate.Op.NOT_EQUALS) return 0;
+            if(op == Predicate.Op.GREATER_THAN || op == Predicate.Op.GREATER_THAN_OR_EQ){
+                if(i == -2) return 0; // 例如(1, 10)查询 >12 的情况
+                else i = 0;
+            }
+            if(op == Predicate.Op.LESS_THAN || op == Predicate.Op.LESS_THAN_OR_EQ){
+                if(i == -1) return 0;
+                else i = numBuckets-1;
+            }
+        }
+
+        // 经过上面处理后switch里就比较单纯了
+        switch (op) {
             case EQUALS:
-                selectivity = (double)bucket.height / (bWidth * ntups);
+                selectivity = (double) histData[i].height / (bWidth * ntups);
                 break;
             case GREATER_THAN:
-                temp = (double)bucket.height/ntups * (bucket.right-v)/bWidth;
-                if(bucket.next == null){ // 例如(1, 10)查询 >12 的情况
-                    selectivity = 0;
-                }else {
-                    bucket = bucket.next;
-                    while (bucket != histTail) {
-                        temp += (double) bucket.height / ntups;
-                        bucket = bucket.next;
-                    }
-                    selectivity = temp;
+                temp = (double) histData[i].height / ntups * (histData[i].right - v) / bWidth;
+                for (i++; i < numBuckets; i++) {
+                    temp += (double) histData[i].height / ntups;
                 }
+                selectivity = temp;
                 break;
             case GREATER_THAN_OR_EQ: // > 和 = 相加
-                temp = (double)bucket.height / (bWidth * ntups);
-                temp += (double)bucket.height/ntups * (bucket.right-v)/bWidth;
-                if(bucket.next == null){
-                    selectivity = 0;
-                }else {
-                    bucket = bucket.next;
-                    while (bucket != histTail) {
-                        temp += (double) bucket.height / ntups;
-                        bucket = bucket.next;
-                    }
-                    selectivity = temp;
+                temp = (double) histData[i].height / (bWidth * ntups);
+                temp += (double) histData[i].height / ntups * (histData[i].right - v) / bWidth;
+                for (i++; i < numBuckets; i++) {
+                    temp += (double) histData[i].height / ntups;
                 }
+                selectivity = temp;
                 break;
             case LESS_THAN:
-                temp = (double)bucket.height/ntups * (v-bucket.left)/bWidth;
-                if(bucket.pre == null){
-                    selectivity = 0;
-                }else {
-                    bucket = bucket.pre;
-                    while (bucket != histHead) {
-                        temp += (double) bucket.height / ntups;
-                        bucket = bucket.pre;
-                    }
-                    selectivity = temp;
+                temp = (double) histData[i].height / ntups * (v - histData[i].left) / bWidth;
+                for (i--; i >= 0; i--) {
+                    temp += (double) histData[i].height / ntups;
                 }
+                selectivity = temp;
                 break;
             case LESS_THAN_OR_EQ:
-                temp = (double)bucket.height / (bWidth * ntups);
-                temp += (double)bucket.height/ntups * (v-bucket.left)/bWidth;
-                if(bucket.pre == null){
-                    selectivity = 0;
-                }else {
-                    bucket = bucket.pre;
-                    while (bucket != histHead) {
-                        temp += (double) bucket.height / ntups;
-                        bucket = bucket.pre;
-                    }
-                    selectivity = temp;
+                temp = (double) histData[i].height / (bWidth * ntups);
+                temp += (double) histData[i].height / ntups * (v - histData[i].left) / bWidth;
+                for (i--; i >= 0; i--) {
+                    temp += (double) histData[i].height / ntups;
                 }
+                selectivity = temp;
                 break;
             case NOT_EQUALS: // 1 - Selectivity(=)
-                selectivity = 1 - (double)bucket.height / (bWidth * ntups);
+                selectivity = 1 - (double) histData[i].height / (bWidth * ntups);
                 break;
         }
         return selectivity;
@@ -197,10 +174,9 @@ public class IntHistogram {
     {
         // some code goes here
         double temp = 0;
-        Bucket current = histHead.next;
-        while(current != histTail){
-            temp += (double)current.height / ntups;
-            current = current.next;
+        for(int i=0; i<numBuckets; i++){
+            Bucket bucket = histData[i];
+            temp += (double)bucket.height / ntups;
         }
         return temp / numBuckets;
     }
@@ -211,10 +187,15 @@ public class IntHistogram {
     public String toString() {
         // some code goes here
         StringBuilder sb = new StringBuilder();
-        Bucket current = histHead.next;
-        while(current != histTail){
-            sb.append("["+current.left+","+current.right+"): "+current.height+"\n");
-            current = current.next;
+        for(int i=0; i<numBuckets; i++){
+            Bucket bucket = histData[i];
+            sb.append("[")
+                    .append(bucket.left)
+                    .append(",")
+                    .append(bucket.right)
+                    .append("): ")
+                    .append(bucket.height)
+                    .append("\n");
         }
         return sb.toString();
     }
